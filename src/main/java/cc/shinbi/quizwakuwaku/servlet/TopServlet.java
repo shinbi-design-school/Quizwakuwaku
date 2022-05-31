3c1e55d60596bcfd1a3af778a7a014a887755845
mitsunori <r.c.m.s.tab@gmail.com>
2022-05-31 10:16:12 +0900

    # WARNING: head commit changed in the meantime
    最初のコミット

--- src/main/java/cc/shinbi/quizwakuwaku/servlet/TopServlet.java
+++ src/main/java/cc/shinbi/quizwakuwaku/servlet/TopServlet.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpSession;
 public class TopServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
+	
 	public TopServlet() {
         super();
 	}
